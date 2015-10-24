d370cef419adaf874d87c808246ef1580d19d5e5
Nathan Wallace <techietotoro@gmail.com>
2015-10-23 18:26:39 -0700

    Initial Commit

---
+++ MobilePitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobilePitch
+//
+//  Created by Nathan Wallace on 10/23/15.
+//  Copyright © 2015 Spark Dev Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobilePitch/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  MobilePitch
+//
+//  Created by Nathan Wallace on 10/23/15.
+//  Copyright © 2015 Spark Dev Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
