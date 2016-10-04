e62bc73aa67b29ffe3623a6762f1cf5e3eeef4f6
Nathan Wallace <techietotoro@gmail.com>
2016-10-04 11:51:49 -0700

    Creates FinishedViewController

---
+++ MobilePitch/FinishedViewController.h
@@ -0,0 +1,13 @@
+//
+//  FinishedViewController.h
+//  MobilePitch
+//
+//  Created by Nathan Wallace on 10/4/16.
+//  Copyright © 2016 Spark Dev Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FinishedViewController : UIViewController
+
+@end
